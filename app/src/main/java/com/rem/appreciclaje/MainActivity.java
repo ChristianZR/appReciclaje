f46dc8d81d3df9a02d1122f727378e0bf7576eeb
Christian <83610281+ChristianZR@users.noreply.github.com>
2022-05-31 01:11:37 -0500

    Creacion del proyecto

---
+++ app/src/main/java/com/rem/appreciclaje/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rem.appreciclaje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
